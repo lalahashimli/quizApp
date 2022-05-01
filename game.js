13bd41d57ee2e609c0d29233a7fafd6945a02913
Lala <lalahashimli@mail.ru>
2022-05-01 16:34:50 +0400

    head's up display and progress bar'

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 
     const question = document.getElementById("question") ;
     const choices = Array.from(document.getElementsByClassName("choice-text"))
+    const progressText = document.getElementById("progressText") ;
+    const scoreText = document.getElementById("score")
+    const progressBarFull = document.getElementById("progressBarFull") ;
 
     let currentQuestion = {} ;
     let acceptingAnswers = false ;
@@ -57,6 +60,12 @@
 
 
         questionCounter ++;
+        progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+
+        // Update the progress bar
+     
+        progressBarFull.style.width = (questionCounter / MAX_QUESTIONS) * 100 + "%" ; 
+
         const questionIndex = Math.floor(Math.random() * availableQuestions.length) ;
         currentQuestion = availableQuestions[questionIndex] ;
         question.innerHTML = currentQuestion.question ;
@@ -79,9 +88,31 @@
           acceptingAnswers = false ;
           const selectedChoice = e.target;
           const selectedAnswer = selectedChoice.dataset["number"];
-          getNewQuestion();
-        })
-    })
+
+
+            const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect' ;
+          
+            if(classToApply === 'correct') {
+                incrementScore(CORRECT_BONUS);
+            }
+
+
+
+            selectedChoice.parentElement.classList.add(classToApply);
+             
+            setTimeout( ()=> {
+            selectedChoice.parentElement.classList.remove(classToApply)
+
+             getNewQuestion();
+            },1000);
+            
+       
+        }) ;
+    }) ;
+
+        incrementScore = num => {
+         score += num ;
+         scoreText.innerText = score ;   }
 
 
     startGame()
